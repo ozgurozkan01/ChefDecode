c83065131eb1e15f20e55a7bba9f41f4f7b6710b
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-10-14 16:51:11 +0300

    add sql server requirements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.chefdecode {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires com.microsoft.sqlserver.jdbc;
 
 
     opens com.example.chefdecode to javafx.fxml;
