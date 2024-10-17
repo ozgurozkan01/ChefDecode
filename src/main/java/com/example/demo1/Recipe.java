bd11f83ea79fe5122b08b8409b42fe5eac41cac3
Ubeyd Gür <ubeydgur41@gmail.com>
2024-10-17 18:17:39 +0300

    add variables of the Recipe class

--- src/main/java/com/example/demo1/Recipe.java
+++ src/main/java/com/example/demo1/Recipe.java
@@ -3,38 +3,60 @@ package com.example.demo1;
 public class Recipe
 {
     private String name;
-    private String rate;
+    private String category;
+    private int preparationTime;
+    private String instruction;
+    private int numberPoints;
+    private float totalPoints;
+    private float rate;
     private String imgSrc;
     private String starImgSrc;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
+    public Recipe(String name, String category, int preparationTime, String instruction, int numberPoints, float totalPoints) {
         this.name = name;
+        this.category = category;
+        this.preparationTime = preparationTime;
+        this.instruction = instruction;
+        this.numberPoints = numberPoints;
+        this.totalPoints = totalPoints;
+        this.rate = totalPoints / numberPoints;
     }
 
-    public String getRate() {
-        return rate;
-    }
 
-    public void setRate(String rate) {
-        this.rate = rate;
-    }
+    public String getName() {return name;}
 
-    public String getImgSrc() {
-        return imgSrc;
-    }
+    public void setName(String name) {this.name = name;}
 
-    public void setImgSrc(String imgSrc) {
-        this.imgSrc = imgSrc;
-    }
+    public String getCategory() {return category;}
 
-    public void setStarImgSrc(String starImgSrc) {
-        this.starImgSrc = starImgSrc;
-    }
-    public String getStarImgSrc() {
-        return starImgSrc;
-    }
+    public void setCategory(String category) {this.category = category;}
+
+    public int getPreparationTime() {return preparationTime;}
+
+    public void setPreparationTime(int preparationTime) {this.preparationTime = preparationTime;}
+
+    public String getInstruction() {return instruction;}
+
+    public void setInstruction(String instruction) {this.instruction = instruction;}
+
+    public int getNumberPoints() {return numberPoints;}
+
+    public void setNumberPoints(int numberPoints) {this.numberPoints = numberPoints;}
+
+    public float getTotalPoints() {return totalPoints;}
+
+    public void setTotalPoints(float totalPoints) {this.totalPoints = totalPoints;}
+
+    public float getRate() {return rate;}
+
+    public void setRate(float rate) {this.rate = rate;}
+
+    public String getImgSrc() {return imgSrc;}
+
+    public void setImgSrc(String imgSrc) {this.imgSrc = imgSrc;}
+
+    public void setStarImgSrc(String starImgSrc) {this.starImgSrc = starImgSrc;}
+
+    public String getStarImgSrc() {return starImgSrc;}
 }
