727925aa5b082b20679c07b3c3a1003bf61dcd66
Ubeyd Gür <ubeydgur41@gmail.com>
2024-10-17 18:12:52 +0300

    add data extraction from database

--- src/main/java/com/example/demo1/Database.java
+++ src/main/java/com/example/demo1/Database.java
@@ -1,31 +1,46 @@
 package com.example.demo1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
-    public static void main(String[] args){
-        Connection connection = null;
+    private Connection connect() throws SQLException {
+        Connection connect = null;
 
-        try {
-            String url = "jdbc:sqlite:/Users/ubeydgur/Projects/Java/ChefDecode/src/database/recipes.db";
-            connection = DriverManager.getConnection(url);
+        String url = "jdbc:sqlite:/Users/ubeydgur/Projects/Java/ChefDecode/src/database/recipes.db";
+        connect = DriverManager.getConnection(url);
 
-            if (connection != null) {
-                System.out.println("Veritabanına başarıyla bağlandınız.");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
+        if (connect != null) {
+            System.out.println("Veritabanına başarıyla bağlandınız.");
         }
+        return connect;
     }
+
+
+    public List<Recipe> getAllRecipes() {
+        List<Recipe> recipesList = new ArrayList<>();
+        String query = "SELECT * FROM recipes";
+
+        try (Connection connection = this.connect();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("RecipeName");
+                String category = resultSet.getString("Category");
+                int preparationTime = resultSet.getInt("PreparationTime");
+                String instruction = resultSet.getString("Instruction");
+                int numberPoints = resultSet.getInt("NumberPoints");
+                float totalPoints = resultSet.getFloat("TotalPoints");
+
+                Recipe recipe = new Recipe(name, category, preparationTime, instruction, numberPoints, totalPoints);
+                recipesList.add(recipe);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return recipesList;
+    }
+
 }
